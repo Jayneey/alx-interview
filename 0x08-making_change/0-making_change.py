0d0b5de8d14ae82cd6531411fa0a732c225da2ea
Jane Affam <75577542+Jayneey@users.noreply.github.com>
2023-05-19 20:55:22 -0700

    Update 0-making_change.py

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,44 +1,24 @@
 #!/usr/bin/python3
 """Change making module.
 """
-def make_change(coins, total):
-  """
-  Given a pile of coins of different values, determine the fewest number of coins needed to meet a given amount total.
 
-  Args:
-    coins: A list of the values of the coins in your possession.
-    total: The amount of change you need to make.
-
-  Returns:
-    The fewest number of coins needed to meet total.
-    If total is 0 or less, return 0.
-    If total cannot be met by any number of coins you have, return -1.
-
-  Raises:
-    ValueError: If coins is not a list of integers.
-
-  """
-
-  if not isinstance(coins, list):
-    raise ValueError("coins must be a list of integers")
-
-  if not all(isinstance(coin, int) for coin in coins):
-    raise ValueError("coins must be a list of integers")
-
-  if total < 0:
-    return -1
-
-  if total == 0:
-    return 0
-
-  coins.sort(reverse=True)
-
-  min_coins = float("inf")
-
-  for coin in coins:
-    if total >= coin:
-      new_min_coins = make_change(coins, total - coin) + 1
-      if new_min_coins < min_coins:
-        min_coins = new_min_coins
-
-  return min_coins
+def makeChange(coins, total):
+    """Determines the fewest number of coins needed to meet a given
+    amount total when given a pile of coins of different values.
+    """
+    if total <= 0:
+        return 0
+    rem = total
+    coins_count = 0
+    coin_idx = 0
+    sorted_coins = sorted(coins, reverse=True)
+    n = len(coins)
+    while rem > 0:
+        if coin_idx >= n:
+            return -1
+        if rem - sorted_coins[coin_idx] >= 0:
+            rem -= sorted_coins[coin_idx]
+            coins_count += 1
+        else:
+            coin_idx += 1
+    return coins_count
